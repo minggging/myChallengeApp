57fa9de7ef2377f692b24fc94dcf68506a66c77a
minggging <109min1004@naver.com>
2022-11-08 21:49:36 +0900

    Initial Commit

---
+++ myChallengeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myChallengeApp
+//
+//  Created by 정민경 on 2022/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
